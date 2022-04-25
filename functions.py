cd2105ce8c6c8bbd012e5a49bd07f28c6e299952
samy@samiordif <samy.difallah@etu.univ-grenoble-alpes.fr>
2022-04-25 15:23:43 +0200

    Fonctions auxiliaires

--- functions.py
+++ functions.py
@@ -4,12 +4,30 @@ Fichier aux.py
 Fonctions auxiliaires
 """
 
+
 import os
+
+
+# Portes logiques
+gates = ["AND", "OR", "k/n"]
+
 
 """
 Efface le terminal
 """
 clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+"""
+
+"""
+def private_from(list1, list2):
+    rev_list = []
+
+    for element in list1:
+        if not (element in list2):
+            rev_list.append(element)
+
+    return rev_list
 
 """
 Entrée :
@@ -39,3 +57,128 @@ def generic_type_input(message, type_input):
     finally:
         # Renvoi du résultat (erreur ou non)
         return user_input
+
+"""
+
+"""
+def get_elements_gate_comb(gate):
+    elements_split = gate.split("/")
+
+    # Erreur si le format de la porte logique n'est pas valide
+    if len(elements_split) != 2:
+        return "\0"
+
+    str_k = elements_split[0]
+    str_n = elements_split[1]
+
+    return (str_k, str_n)
+
+"""
+Entrée :
+    gate (chaine de caractères) : porte logique d'un noeud
+    children (entier) : nombre de fils d'un noeud
+Sortie : booléen
+
+Renvoie vrai ssi la porte logique gate est valide (méthode utile pour la saisie utilisateur)
+"""
+def gate_is_valid(gate, children):
+    # La porte logique gate est répertoriée dans la liste gates : il s'agit soit d'une porte AND, soit d'une porte OR
+    if gate in gates:
+        return True
+
+    # La porte logique contient le caractère '/' : il s'agit éventuellement d'une porte logique k/n
+    if "/" in gate:
+        # Vérification de la validité de la porte logique
+
+        split_gate = get_elements_gate_comb(gate)
+
+        if split_gate != "\0":
+            str_k = split_gate[0]
+            str_n = split_gate[1]
+
+            # Les deux éléments k et n doivent être numériques
+            if str_k.isnumeric() and str_n.isnumeric():
+                n = int(str_n)
+
+                # n doit être égal au nombre de fils du noeud courant
+                if n == children:
+                    return True
+
+    return False
+
+"""
+Entrée :
+    rule (chaine de caractères) : règle de calcul
+Sortie : booléen
+
+Renvoie vrai ssi la règle de calcul rule est valide (méthode utile pour la saisie utilisateur)
+"""
+def rule_is_valid(gate, rule):
+    # Règles de calcul valides pour les portes logiques AND et k/n : somme et produit
+
+    if gate == "AND" or gate == "k/n":
+        valid_rules_and_comb = ["+", "*"]
+        return (rule in valid_rules_and_comb)
+
+    # Règles de calcul valides pour la porte logique OR : maximum et minimum
+
+    valid_rules_or = ["M", "m"]
+    return (rule in valid_rules_or)
+
+"""
+Entrée :
+    optimization (chaine de caractères) : règle d'opitmisation
+Sortie : booléen
+
+Renvoie vrai ssi la règle d'optimisation optimization est valide (méthode utile pour la saisie utilisateur)
+"""
+def optimization_is_valid(optimization):
+    # Maximisation ou minimisation
+
+    valid_optimizations = ["M", "m"]
+    return (optimization in valid_optimizations)
+
+"""
+Entrée :
+    gate (chaine de caractères) : porte logique d'un noeud
+    set_n (entier) : nouvelle valeur de n pour la combinaison k parmi n
+Sortie : chaine de caractères
+
+Modifie et renvoie une porte de type k parmi n
+"""
+def set_gate_comb(gate, set_n):
+    (str_k, str_n) = get_elements_gate_comb(gate)
+
+    n = int(str_n)
+    n += set_n
+    str_n = str(n)
+
+    gate = str_k + "/" + str_n
+
+    return gate
+
+"""
+
+"""
+def list_gate_comb(list, k):
+    lgc = []
+    i = 0
+    i_max = 2**len(list)-1
+
+    while i <= i_max:
+        tmp_list = []
+        j = 0
+        j_max = len(list)-1
+
+        while j <= j_max:
+            if (i >> j) & 1 == 1:
+                tmp_list.append(list[j])
+
+            j += 1
+
+        if len(tmp_list) == k:
+            lgc.append(tmp_list)
+
+        i += 1
+
+    return lgc
